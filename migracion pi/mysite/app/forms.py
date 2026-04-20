c1765701e14ea5a39ada4aaa0f2631d62a38e870
nickinicole1365-cpu <nickinicole1365@gmail.com>
2026-04-20 10:13:05 -0600

    migration a Django
    nueva carpeta

---
+++ migracion pi/mysite/app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Post
+
+
+class NewPost(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['image','titulo','autor','link','texto']
+        labels = {
+            'image': '',
+            'titulo': '',
+            'autor': '',
+            'link': '',
+            'texto': '',
+        }
+
+
